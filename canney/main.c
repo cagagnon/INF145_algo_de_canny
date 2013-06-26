ea0aa57109d1d3c1b5a5dddf0a780da74f2e2330
Charles-Alexandre Gagnon <charlesalexandregagnon@Charles-Alexandre-Gagnons-MacBook-Pro.local>
2013-06-26 18:28:30 -0400

    Initial Commit

---
+++ canney/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  canney
+//
+//  Created by Charles-Alexandre Gagnon on 2013-06-26.
+//  Copyright (c) 2013 ETS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
